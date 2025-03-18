db4c78fcfd8add1673bdb61f1556491fc31b468d
LAPTOP-1DQNJ8L3\aleja <339210@alu.murciaeduca.es>
2025-03-19 00:28:20 +0100

    Paso 2. Creación de la tabla images

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'body',
+        'visibility',
+        'likes',
+        'comments_count',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function images()
+    {
+        return $this->hasMany(Image::class);
+    }
 }
